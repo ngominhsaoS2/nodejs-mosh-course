b3f798b2478ffa67209d926285c95df8b5bd03b8
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-12 13:02:07 +0700

    17. OS Module

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,6 @@
-const path = require('path');
+const os = require('os');
 
-var pathObj = path.parse(__filename);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 
-console.log(pathObj);
+console.log({ totalMemory, freeMemory })
