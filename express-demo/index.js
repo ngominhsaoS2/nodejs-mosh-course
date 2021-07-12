c74b74bca5b2b50b1ed9292d1a756b19196e8ef6
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-12 22:59:01 +0700

    48. Handling HTTP GET Requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'NodeJs' },
+    { id: 2, name: 'Vue' },
+    { id: 3, name: 'React' }
+]
+
 app.get('/', (req, res) => {
     res.send('Hello, world');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
-app.get('/api/courses/:year/:month', (req, res) => {
-    res.send(req.params);
-    //res.send(req.query);
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given Id was not found.');
+    res.send(course);
 });
-
-
 
 // PORT
 const port = process.env.PORT || 3000;
