ff95ddfa0e7ef6ad7c0f34114669da04adca99e2
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-12 15:49:26 +0700

    20. Event Arguments

--- first-app/app.js
+++ first-app/app.js
@@ -2,9 +2,9 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged', function() {
-    console.log('Message just got logged');
+emitter.on('messageLogged', (arg) => {
+    console.log('Message just got logged', arg);
 });
 
 // Raise an event names "messageLogged"
-emitter.emit('messageLogged'); 
+emitter.emit('messageLogged', { id: 1, name: 'something', url: 'https://' }); 
