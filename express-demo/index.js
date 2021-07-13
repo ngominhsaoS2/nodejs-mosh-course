c5b8a97a0ca865bde24d630b57e47f5dee64e3e6
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-13 16:09:52 +0700

    59. Built-In Middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,6 +4,8 @@ const Joi = require('joi'); // this is a class
 const logger = require('./logger');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.use(logger);
 
