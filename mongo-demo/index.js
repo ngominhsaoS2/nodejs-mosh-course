85f05309f28fc5c20964871b57f3de4e67ddc00a
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-13 17:37:05 +0700

    86. Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to Mongodb'))
     .catch(err => console.error('Could not connect to Mongodb: ', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+})
