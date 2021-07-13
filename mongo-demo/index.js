6fe0dfed880bf40804d17c5c33774fc309aa360d
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-13 18:45:32 +0700

    94. Pagination

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -27,9 +27,13 @@ async function createCourse() {
 }
 
 async function getCourses() {
+    const pageNumber = 2;
+    const pageSize = 10;
+
     const courses = await Course
         .find({ author: /.*sao.*/i }) // Contains with sao
-        .limit(10)
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
         .sort({ name: 1 }) // 1 asc -1 desc
         .count()
     console.log(courses);
